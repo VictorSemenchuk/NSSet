33224e8d3523aad9bad30bb0ed9fd1c990e0d852
Victor Semenchuk <semenchuk.vitk@gmail.com>
2018-04-14 11:00:49 +0300

    NSSet Task

---
+++ NSSetTask/NSSetTask/ViewController.m
@@ -0,0 +1,57 @@
+//
+//  ViewController.m
+//  NSSetTask
+//
+//  Created by Victor Macintosh on 14/04/2018.
+//  Copyright © 2018 Victor Semenchuk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //MARK: Create NSArray, containing 30 objects. Add duplicates to array.
+    NSArray *startArray = @[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10",
+                            @"1", @"12", @"3", @"14", @"15", @"16", @"7", @"8", @"19", @"20",
+                            @"21", @"2", @"23", @"4", @"5", @"26", @"27", @"28", @"29", @"10"];
+    //We have 8 duplicates in the array
+    
+    //MARK: Use NSSet to exclude duplicates from array.
+    NSSet *set = [NSSet setWithArray:startArray];
+    
+    //MARK: Check what is faster: Create an array of 100 numbers. Check whether number 74 is contained inside an array. Transform array into NSSet and check whether number 74 is contained inside NSSet.
+    
+    //Checking for array
+    NSMutableArray *mutableArray100 = [[NSMutableArray alloc] init];
+    for(int i = 1; i <= 100; i++) {
+        [mutableArray100 addObject:[NSNumber numberWithInteger:i]];
+    }
+    [mutableArray100 enumerateObjectsUsingBlock:^(NSNumber *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([obj integerValue] == 74) {
+            *stop = YES;
+        }
+    }];
+    
+    //Checking for set
+    NSSet *set100 = [NSSet setWithArray:mutableArray100];
+    [set100 containsObject:[NSNumber numberWithInteger:74]];
+    
+    //Releasing
+    [mutableArray100 release];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
